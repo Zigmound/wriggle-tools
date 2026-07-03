91bdb09581d37aba318317ecb79d1a054f1e37f2
Zigmound <nathanielpeterson08@gmail.com>
2026-07-03 09:43:08 -0400

    Add a program which computes HP and MP as a function of skill and XL
    As a happy byproduct of writing crawl_optimal_skilling, we now have
    functions `get_real_hp` and `get_real_mp` which compute HP and MP
    given skill levels and XL. `hp_mp_breakpoints.py` displays that
    information in table form, allowing you to see the skill level
    breakpoints. It's a lot weirder than I expected.

---
+++ hp_mp_breakpoints.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import sys
+import numpy as np
+import pandas as pd
+import crawl_tab as CT
+import crawl_skills as CS
+
+pd.options.display.width = sys.maxsize
+pd.options.display.max_columns = None
+pd.options.display.max_rows = sys.maxsize
+
+if __name__ == "__main__":
+    race = "Human"
+    you = {
+        "skills": {"Fighting": 0, "Spellcasting": 3.3, "Invocations": 0},
+        "XL": 1,
+        "HP_bonus": 0,
+        "HP_multiplier": 1,
+        "MP_bonus": 0,
+        "brand": "none",
+        "race": race,
+    }
+    skill_levels = np.arange(0, 27.1, 0.1)
+    XL = range(1, 28)
+
+    CS.you = CS.Player(race)
+
+    for skl, stat, calc in [
+        ("Fighting", "HP", CT.get_real_hp),
+        ("Spellcasting", "MP", CT.get_real_mp),
+    ]:
+        data = list()
+        print(f"{race} {skl} breakpoints (columns represent XL)")
+        for xli in XL:
+            you["XL"] = xli
+            previous = 0
+            for lvl in skill_levels:
+                you["skills"][skl] = lvl
+                statval = calc(you)
+                if statval > previous:
+                    data.append((xli, lvl, statval))
+                    previous = statval
+        df = pd.DataFrame(data, columns=["XL", skl, stat])
+        df = df.pivot_table(index=skl, columns="XL", values=stat)
+        df = df.fillna(-1).astype(int).astype(str)
+        for col in df:
+            df[col] = df[col].str.replace("-1", "")
+        print(df.to_markdown(), end="\n" * 2)
